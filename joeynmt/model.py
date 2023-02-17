3d6c57735e29961a9f6b43448201632ef4e9bbf1
ciela <zektbach@gmail.com>
2023-02-17 12:34:34 +0900

    Rename `soft_beam_search` to `soft_beam_policy`

--- joeynmt/model.py
+++ joeynmt/model.py
@@ -256,7 +256,7 @@ class Model(nn.Module):
                 Qs_to_return, rewards, mrt=True, samples=samples)) \
                 if log_probabilities else (batch_loss, [])
 
-    def soft_beam_search(self, max_output_length, src: Tensor, trg: Tensor, src_mask: Tensor,
+    def soft_beam_policy(self, max_output_length, src: Tensor, trg: Tensor, src_mask: Tensor,
             src_length: Tensor, temperature: float, topk: int, log_probabilities: False, pickle_logs:False,
             alpha: float = 1., gumbel_scale: float = 1., max_adoption_size: int = 100, beam_size: int = 5):
         """ Computes forward pass for Soft Beam Search
@@ -833,7 +833,7 @@ class Model(nn.Module):
             return_tuple = (loss, logging, None, None)
 
         elif return_type == "sbs":
-            loss, logging = self.soft_beam_search(
+            loss, logging = self.soft_beam_policy(
             src=kwargs["src"],
             trg=kwargs["trg"],
             src_mask=kwargs["src_mask"],
